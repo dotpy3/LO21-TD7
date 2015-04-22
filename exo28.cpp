7726a261db7dccf87fa8cbbaa26e57b9f8922ddf
nxp3 <moppapo@gmail.com>
2015-04-22 10:38:12 +0200

    Ajout de l'exo 28

--- exo28.cpp
+++ exo28.cpp
@@ -1,1 +1,16 @@
+#include "entete.h"
+#include <QLineEdit>
 
+int main(int argc, char* argv[]){
+    QApplication app2(argc, argv);
+
+    QWidget fenetre;
+    fenetre.setFixedSize(200,200);
+
+    QLineEdit identificateur(&fenetre);
+    identificateur.move(10,10);
+
+    fenetre.show();
+
+    return app2.exec();
+}
