ab0dd3ae73bf31e0e73e831059dc2905411ac7f7
nxp3 <moppapo@gmail.com>
2015-04-22 10:55:20 +0200

    Question 2 de l'exercice 28

--- exo28.cpp
+++ exo28.cpp
@@ -1,6 +1,7 @@
 #include "entete.h"
 #include <QLineEdit>
 #include <QTextEdit>
+#include <QHBoxLayout>
 
 int main(int argc, char* argv[]){
     QApplication app2(argc, argv);
@@ -8,7 +9,11 @@ int main(int argc, char* argv[]){
     QWidget fenetre;
     fenetre.setFixedSize(200,200);
 
-    QLineEdit identificateur(&fenetre);
+    /* Question 1 :
+     *
+     * On positionne les éléments de façon absolue
+     *
+     * QLineEdit identificateur(&fenetre);
     identificateur.move(10,10);
 
     QTextEdit titre(&fenetre);
@@ -17,7 +22,28 @@ int main(int argc, char* argv[]){
 
     QPushButton save("Sauver",&fenetre);
     save.move(10,170);
-    save.setFixedWidth(80);
+    save.setFixedWidth(80); */
+
+    /* Question 2 :
+     *
+     * On crée un layout, QVBoxLayout *layout,
+     * qui se chargera d'agencer les éléments automatiquement de façon
+     * verticale.
+     * (il existe QHBoxLayout pour l'agencement horizontal)
+     *
+     * */
+
+    QLineEdit* identificateur = new QLineEdit();
+    QTextEdit* titre = new QTextEdit();
+    QPushButton* save = new QPushButton("Sauver");
+
+    QVBoxLayout *layout= new QVBoxLayout;
+
+    layout->addWidget(identificateur);
+    layout->addWidget(titre);
+    layout->addWidget(save);
+
+    fenetre.setLayout(layout);
 
     fenetre.show();
 
